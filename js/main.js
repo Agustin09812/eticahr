0057fcfa8a5037fa12f1045318469c63b67cc98f
Agustin09812 <agustinivanlago@gmail.com>
2024-11-04 11:33:58 -0300

    first push

---
+++ js/main.js
@@ -0,0 +1,116 @@
+(function ($) {
+    "use strict";
+
+    /**
+   * Apply .scrolled class to the body as the page is scrolled down
+   */
+    function toggleScrolled() {
+        const selectBody = document.querySelector('body');
+        const selectHeader = document.querySelector('#header');
+        if (!selectHeader.classList.contains('scroll-up-sticky') && !selectHeader.classList.contains('sticky-top') && !selectHeader.classList.contains('fixed-top')) return;
+        window.scrollY > 100 ? selectBody.classList.add('scrolled') : selectBody.classList.remove('scrolled');
+    }
+
+    document.addEventListener('scroll', toggleScrolled);
+    window.addEventListener('load', toggleScrolled);
+
+    /**
+  * Mobile nav toggle
+  */
+    const mobileNavToggleBtn = document.querySelector('.mobile-nav-toggle');
+
+    function mobileNavToogle() {
+        document.querySelector('body').classList.toggle('mobile-nav-active');
+        mobileNavToggleBtn.classList.toggle('bi-list');
+        mobileNavToggleBtn.classList.toggle('bi-x');
+    }
+    mobileNavToggleBtn.addEventListener('click', mobileNavToogle);
+
+    /**
+     * Hide mobile nav on same-page/hash links
+     */
+    document.querySelectorAll('#navmenu a').forEach(navmenu => {
+        navmenu.addEventListener('click', () => {
+            if (document.querySelector('.mobile-nav-active')) {
+                mobileNavToogle();
+            }
+        });
+
+    });
+
+    /**
+     * Toggle mobile nav dropdowns
+     */
+    document.querySelectorAll('.navmenu .toggle-dropdown').forEach(navmenu => {
+        navmenu.addEventListener('click', function (e) {
+            e.preventDefault();
+            this.parentNode.classList.toggle('active');
+            this.parentNode.nextElementSibling.classList.toggle('dropdown-active');
+            e.stopImmediatePropagation();
+        });
+    });
+
+    // Spinner
+    var spinner = function () {
+        setTimeout(function () {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner(0);
+
+
+    // Initiate the wowjs
+    new WOW().init();
+
+    // Header carousel
+    $(".header-carousel").owlCarousel({
+        items: 1,
+        dots: true,
+        touchDrag: false,
+        mouseDrag: false
+    });
+
+    /**
+  * Correct scrolling position upon page load for URLs containing hash links.
+  */
+    window.addEventListener('load', function (e) {
+        if (window.location.hash) {
+            if (document.querySelector(window.location.hash)) {
+                setTimeout(() => {
+                    let section = document.querySelector(window.location.hash);
+                    let scrollMarginTop = getComputedStyle(section).scrollMarginTop;
+                    window.scrollTo({
+                        top: section.offsetTop - parseInt(scrollMarginTop),
+                        behavior: 'smooth'
+                    });
+                }, 100);
+            }
+        }
+    });
+
+    /**
+   * Navmenu Scrollspy
+   */
+    let navmenulinks = document.querySelectorAll('.navmenu a');
+
+    function navmenuScrollspy() {
+        navmenulinks.forEach(navmenulink => {
+            if (!navmenulink.hash) return;
+            let section = document.querySelector(navmenulink.hash);
+            if (!section) return;
+            let position = window.scrollY + 200;
+            if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+                document.querySelectorAll('.navmenu a.active').forEach(link => link.classList.remove('active'));
+                navmenulink.classList.add('active');
+            } else {
+                navmenulink.classList.remove('active');
+            }
+        })
+    }
+    window.addEventListener('load', navmenuScrollspy);
+    document.addEventListener('scroll', navmenuScrollspy);
+
+})(jQuery);
+
